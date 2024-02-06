5e2c7130af8652be5c59c8f8741767eda878732c
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-02-06 13:11:38 +0300

    Generic repository design pattern configuration done.

---
+++ Core/HireACar.Application/Abstract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HireACar.Domain.Entities;
+
+namespace HireACar.Application.Abstract
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> Delete(T entity);
+    }
+}
---
+++ Infrastructure/HireACar.Persistance/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HireACar.Application.Abstract;
+using HireACar.Domain.Entities;
+using HireACar.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace HireACar.Persistance.Concrete.Repositories
+{
+    public class GenericRepository<T>:IRepository<T> where T : BaseEntity
+    {
+        private readonly HireACarContext _context;
+
+        public GenericRepository(HireACarContext context)
+        {
+            _context = context;
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var result = await _context.Set<T>().FindAsync(id);
+            return result ?? null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;  
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
