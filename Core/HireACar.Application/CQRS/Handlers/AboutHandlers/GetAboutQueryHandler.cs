b07507dfb7b732cb788192fc41d72dba3be9f8d6
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-02-07 13:35:13 +0300

    CQRS design pattern configuration done.

---
+++ Core/HireACar.Application/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HireACar.Application.Abstract;
+using HireACar.Application.CQRS.Queries.AboutQueries;
+using HireACar.Application.CQRS.Results.AboutResults;
+using HireACar.Domain.Entities;
+
+namespace HireACar.Application.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+        private readonly IMapper _mapper;
+        public GetAboutQueryHandler(IRepository<About> repository,IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetAboutQueryResult> Handle(GetAboutQuery request)
+        {
+            var about = await _repository.GetByIdAsync(1);
+            var mappingAbout = _mapper.Map<GetAboutQueryResult>(about);
+            return mappingAbout;
+        }
+    }
+}
---
+++ Core/HireACar.Application/Mappings/AboutMapping/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HireACar.Application.CQRS.Results.AboutResults;
+using HireACar.Domain.Entities;
+
+namespace HireACar.Application.Mappings.AboutMapping
+{
+    public class MappingProfiles:Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<About, GetAboutQueryResult>().ReverseMap();
+        }
+    }
+}
