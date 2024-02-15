5dc4343a23d48e9623295f5c21e27e02b715b725
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-02-15 16:00:41 +0300

    Added column IsApproved and IsDeleted in car table

--- Infrastructure/HireACar.Persistance/Migrations/20240215125023_add-column-IsDeletedAndIsApproved-in-car-table.Designer.cs
+++ Infrastructure/HireACar.Persistance/Migrations/20240215125023_add-column-IsDeletedAndIsApproved-in-car-table.Designer.cs
@@ -4,6 +4,7 @@ using HireACar.Persistance.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HireACar.Persistance.Migrations
 {
     [DbContext(typeof(HireACarContext))]
-    partial class HireACarContextModelSnapshot : ModelSnapshot
+    [Migration("20240215125023_add-column-IsDeletedAndIsApproved-in-car-table")]
+    partial class addcolumnIsDeletedAndIsApprovedincartable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/HireACar.Persistance/Migrations/20240215125023_add-column-IsDeletedAndIsApproved-in-car-table.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HireACar.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcolumnIsDeletedAndIsApprovedincartable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "Cars",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Cars",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Cars");
+        }
+    }
+}
