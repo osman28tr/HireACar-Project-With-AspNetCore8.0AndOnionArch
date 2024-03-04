33ae9e22ce7f9ad7bd64a91c94de8861429890b3
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-03-04 18:40:57 +0300

    Create feature operation done.

---
+++ Core/HireACar.Application/CQRS/Commands/FeatureCommands/CreatedFeatureCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Commands.FeatureCommands
+{
+    public class CreatedFeatureCommand:IRequest
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Core/HireACar.Application/CQRS/Handlers/FeatureHandlers/CommandHandlers/CreatedFeatureCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HireACar.Application.Abstract;
+using HireACar.Application.CQRS.Commands.FeatureCommands;
+using HireACar.CrossCuttingConcerns.Exceptions;
+using HireACar.Domain.Entities;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Handlers.FeatureHandlers.CommandHandlers
+{
+    public class CreatedFeatureCommandHandler:IRequestHandler<CreatedFeatureCommand>
+    {
+        private readonly IFeatureRepository _featureRepository;
+        private readonly IMapper _mapper;
+        public CreatedFeatureCommandHandler(IFeatureRepository featureRepository, IMapper mapper)
+        {
+            _featureRepository = featureRepository;
+            _mapper = mapper;
+        }
+        public async Task Handle(CreatedFeatureCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var feature = _mapper.Map<Feature>(request);
+                await _featureRepository.AddAsync(feature);
+            }
+            catch
+            {
+                throw new CustomInternalServerException("Özellik ekleme sırasında bir hata oluştu.");
+            }
+        }
+    }
+}
