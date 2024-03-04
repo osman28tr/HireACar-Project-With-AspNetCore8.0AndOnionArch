e3a0c1e549c690e4f2463b90fac08c74af6d215d
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-03-04 21:25:22 +0300

    Delete footer operation done.

---
+++ Core/HireACar.Application/CQRS/Commands/FooterCommands/DeletedFooterCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Commands.FooterCommands
+{
+    public class DeletedFooterCommand:IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Core/HireACar.Application/CQRS/Handlers/FooterHandlers/CommandHandlers/DeletedFooterCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HireACar.Application.Abstract;
+using HireACar.Application.CQRS.Commands.FooterCommands;
+using HireACar.CrossCuttingConcerns.Exceptions;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Handlers.FooterHandlers.CommandHandlers
+{
+    public class DeletedFooterCommandHandler:IRequestHandler<DeletedFooterCommand>
+    {
+        private readonly IFooterRepository _footerRepository;
+        public DeletedFooterCommandHandler(IFooterRepository footerRepository)
+        {
+            _footerRepository = footerRepository;
+        }
+        public async Task Handle(DeletedFooterCommand request, CancellationToken cancellationToken)
+        {
+            var footer = await _footerRepository.GetAsync(x => x.Id == request.Id);
+            if (footer == null)
+                throw new NotFoundException("Silmek istediğiniz footer bulunamadı.");
+            try
+            {
+                await _footerRepository.DeleteAsync(footer);
+            }
+            catch
+            {
+                throw new CustomInternalServerException("Footer silme sırasında bir hata oluştu.");
+            }
+        }
+    }
+}
