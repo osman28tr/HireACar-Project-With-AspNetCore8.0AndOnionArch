f9a4b56ab27d799b879a5334225ee9797173723c
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-03-04 21:25:35 +0300

    Update footer operation done.

---
+++ Core/HireACar.Application/CQRS/Commands/FooterCommands/UpdatedFooterCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Commands.FooterCommands
+{
+    public class UpdatedFooterCommand:IRequest
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Core/HireACar.Application/CQRS/Handlers/FooterHandlers/CommandHandlers/UpdatedFooterCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HireACar.Application.Abstract;
+using HireACar.Application.CQRS.Commands.FooterCommands;
+using HireACar.CrossCuttingConcerns.Exceptions;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Handlers.FooterHandlers.CommandHandlers
+{
+    public class UpdatedFooterCommandHandler:IRequestHandler<UpdatedFooterCommand>
+    {
+        private readonly IFooterRepository _footerRepository;
+        private readonly IMapper _mapper;
+        public UpdatedFooterCommandHandler(IFooterRepository footerRepository, IMapper mapper)
+        {
+            _footerRepository = footerRepository;
+            _mapper = mapper;
+        }
+        public async Task Handle(UpdatedFooterCommand request, CancellationToken cancellationToken)
+        {
+            var footer = await _footerRepository.GetAsync(x => x.Id == request.Id);
+            if (footer == null)
+                throw new NotFoundException("Güncellemek istediğiniz footer bulunamadı.");
+            try
+            {
+                _mapper.Map(request, footer);
+                await _footerRepository.UpdateAsync(footer);
+            }
+            catch
+            {
+                throw new CustomInternalServerException("Footer güncelleme sırasında bir hata oluştu.");
+            }
+        }
+    }
+}
