08a0d86854480fa648bb17f9d9547bd19f2c9248
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-03-04 18:41:40 +0300

    Delete feature operation done.

---
+++ Core/HireACar.Application/CQRS/Commands/FeatureCommands/DeletedFeatureCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Commands.FeatureCommands
+{
+    public class DeletedFeatureCommand:IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Core/HireACar.Application/CQRS/Handlers/FeatureHandlers/CommandHandlers/DeletedFeatureCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HireACar.Application.Abstract;
+using HireACar.Application.CQRS.Commands.FeatureCommands;
+using HireACar.CrossCuttingConcerns.Exceptions;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Handlers.FeatureHandlers.CommandHandlers
+{
+    public class DeletedFeatureCommandHandler:IRequestHandler<DeletedFeatureCommand>
+    {
+        private readonly IFeatureRepository _featureRepository;
+        public DeletedFeatureCommandHandler(IFeatureRepository featureRepository)
+        {
+            _featureRepository = featureRepository;
+        }
+        public async Task Handle(DeletedFeatureCommand request, CancellationToken cancellationToken)
+        {
+            var feature = await _featureRepository.GetAsync(x => x.Id == request.Id);
+            if (feature == null)
+                throw new NotFoundException("Silmek istediğiniz özellik bulunamadı.");
+            try
+            {
+                await _featureRepository.DeleteAsync(feature);
+            }
+            catch
+            {
+                throw new CustomInternalServerException("Özellik silme sırasında bir hata oluştu.");
+            }
+        }
+    }
+}
