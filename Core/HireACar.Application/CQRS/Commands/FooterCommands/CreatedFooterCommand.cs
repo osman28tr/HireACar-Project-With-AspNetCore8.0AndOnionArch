2347d98f94f463748a79498553fa682cc6c173b1
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-03-04 21:24:11 +0300

    Create footer operation done.

---
+++ Core/HireACar.Application/CQRS/Commands/FooterCommands/CreatedFooterCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Commands.FooterCommands
+{
+    public class CreatedFooterCommand:IRequest
+    {
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Core/HireACar.Application/CQRS/Handlers/FooterHandlers/CommandHandlers/CreatedFooterCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HireACar.Application.Abstract;
+using HireACar.Application.CQRS.Commands.FooterCommands;
+using HireACar.CrossCuttingConcerns.Exceptions;
+using HireACar.Domain.Entities;
+using MediatR;
+
+namespace HireACar.Application.CQRS.Handlers.FooterHandlers.CommandHandlers
+{
+    public class CreatedFooterCommandHandler:IRequestHandler<CreatedFooterCommand>
+    {
+        private readonly IFooterRepository _footerRepository;
+        private readonly IMapper _mapper;
+        public CreatedFooterCommandHandler(IFooterRepository footerRepository, IMapper mapper)
+        {
+            _footerRepository = footerRepository;
+            _mapper = mapper;
+        }
+        public async Task Handle(CreatedFooterCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var footer = _mapper.Map<Footer>(request);
+                await _footerRepository.AddAsync(footer);
+            }
+            catch
+            {
+                throw new CustomInternalServerException("Footer ekleme işlemi sırasında bir hata oluştu.");
+            }
+        }
+    }
+}
