78fa0619aa035fbf6099bf9489d1573136989218
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-03-25 02:32:30 +0300

    AboutApi is fetched.

---
+++ Frontends/HireACar.UI/ViewComponents/AboutViewComponents/AboutUsComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using HireACar.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HireACar.UI.ViewComponents.AboutViewComponents
+{
+    public class AboutUsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5081/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var response = await responseMessage.Content.ReadAsStringAsync();
+                var abouts = JsonConvert.DeserializeObject<GetAboutDto>(response);
+                return View(abouts);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/HireACar.UI/ViewComponents/AboutViewComponents/BecomeADriverComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HireACar.UI.ViewComponents.AboutViewComponents
+{
+	public class BecomeADriverComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
